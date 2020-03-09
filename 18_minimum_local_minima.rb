f4bb7a5d0ca0051df0a4a6fa46beb19f8d5f6049
carden <carden.ruby@gmail.com>
2020-03-10 00:33:23 +0300

    first version

--- 18_minimum_local_minima.rb
+++ 18_minimum_local_minima.rb
@@ -1,5 +1,5 @@
 # The program checks the array and displays the min value from its local minima.
 arr = [1, 2, 2, -4, 0, 6, 7, 8, 12]
 prod = []
-arr.each_with_index { |_, i| prod << arr[i - 1] if arr[i - 1] < arr[i] }
+arr.each_with_index { |_, ind| prod << arr[ind - 1] if arr[ind - 1] < arr[ind] }
 puts "Minimum value of all local minima: #{prod.min}"
