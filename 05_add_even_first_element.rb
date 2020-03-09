f2c6124318f4d0c034b8ece8a797468f86a752d8
carden <carden.ruby@gmail.com>
2020-03-10 00:03:41 +0300

    first version

---
+++ 05_add_even_first_element.rb
@@ -0,0 +1,9 @@
+# The program converts the array by adding the first element to even numbers.
+# The first and last elements of the array do not change.
+arr = [1, 2, 2, -4, 0, 6, 7, 8, 12]
+arr = arr.each_with_index do |elem, index|
+  if elem.even? && elem != arr.first && elem != arr.last
+    arr[index] += arr.first
+  end
+end
+p arr
