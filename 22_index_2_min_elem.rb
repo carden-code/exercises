02339ad3ac7c043c5398af064f7184ce691d0f51
carden <carden.ruby@gmail.com>
2020-04-04 23:21:56 +0300

    unless

--- 22_index_2_min_elem.rb
+++ 22_index_2_min_elem.rb
@@ -1,4 +1,6 @@
 # The program displays the indices of the two smallest elements of the array.
 arr = [1, 2, 2, -4, 0, 6, 7, 8, -9, 12]
 a, b = arr.min(2)
-arr.each_with_index { |elem, index| puts "#{index}. #{elem}" if a == elem || b == elem }
+arr.each_with_index do |elem, index|
+  puts "#{index}. #{elem}" unless a != elem && b != elem
+end
