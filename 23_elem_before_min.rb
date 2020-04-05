a9616ceae18a84aedf35fb933609730a97628e64
carden <carden.ruby@gmail.com>
2020-04-05 19:50:04 +0300

    fix bugs

--- 23_elem_before_min.rb
+++ 23_elem_before_min.rb
@@ -1,10 +1,9 @@
 # Program find items in front of the last minimum.
 arr = [1, 2, 2, -4, 0, 6, 7, 8, -9, -2, 12]
-# arr = [-1, -1, -1, -1, -1, -1]
 product = []
 arr.each_with_index do |elem, index|
   product << elem
   break if arr.rindex(arr.min) == index
 end
-c.pop
+product.pop
 puts "Elements located before the last minimum: #{product}"
