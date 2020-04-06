d4ef03fa886b535a42dc609cb31e6c7d17373344
carden <carden.ruby@gmail.com>
2020-04-06 19:36:09 +0300

    works

---
+++ 27_reverse_between_min_max.rb
@@ -0,0 +1,21 @@
+# Дан целочисленный массив. Необходимо переставить в обратном порядке элементы
+# массива, расположенные между его минимальным и максимальным элементами.
+arr = [1, -10, 2, -4, 0, 6, 7, 8, -9, 12, -2, 2]
+before_min = []
+after_max = []
+arr.each do |elem|
+  before_min << elem
+  break if elem == arr.min
+end
+arr.reverse_each do |elem|
+  after_max << elem
+  break if elem == arr.max
+end
+
+after_max.reverse!
+arr.pop(after_max.size)
+arr.shift(before_min.size)
+arr.reverse!
+arr.unshift(before_min)
+arr.concat(after_max)
+puts "Product: #{arr.flatten}"
