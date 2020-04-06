53bcff78f946a1214567a40a889bf9d9850f8b8f
carden <carden.ruby@gmail.com>
2020-04-06 20:45:12 +0300

    interval

---
+++ 29_number_elements_interval.rb
@@ -0,0 +1,7 @@
+# Дан целочисленный массив и интервал a..b.
+# Необходимо найти количество элементов в этом интервале.
+# The program finds the number of elements in this interval.
+array = [1, -10, 2, -4, 0, 6, 7, 8, -9, 12, -2, 2]
+interval = 'a'..'b'
+puts "Integer array: #{array}"
+puts "Number of elements in this interval 'a'..'b': #{interval.to_a.size}"
