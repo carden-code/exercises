baa27baf5e93d44ae09db37163b2c7b461bc1a26
carden <carden.ruby@gmail.com>
2020-04-06 19:43:06 +0300

    rename variable

--- 27_reverse_between_min_max.rb
+++ 27_reverse_between_min_max.rb
@@ -1,23 +1,23 @@
 # Дан целочисленный массив. Необходимо переставить в обратном порядке элементы
 # массива, расположенные между его минимальным и максимальным элементами.
-arr = [1, -10, 2, -4, 0, 6, 7, 8, -9, 12, -2, 2]
+array = [1, -10, 2, -4, 0, 6, 7, 8, -9, 12, -2, 2]
 before_min = []
 after_max = []
 
-arr.each do |elem|
-  before_min << elem
-  break unless elem != arr.min
+array.each do |element|
+  before_min << element
+  break unless element != array.min
 end
 
-arr.reverse_each do |elem|
-  after_max << elem
-  break unless elem != arr.max
+array.reverse_each do |element|
+  after_max << element
+  break unless element != array.max
 end
 
 after_max.reverse!
-arr.pop(after_max.size)
-arr.shift(before_min.size)
-arr.reverse!
-arr.unshift(before_min)
-arr.concat(after_max)
-puts "Product: #{arr.flatten}"
+array.pop(after_max.size)
+array.shift(before_min.size)
+array.reverse!
+array.unshift(before_min)
+array.concat(after_max)
+puts "Product: #{array.flatten}"
