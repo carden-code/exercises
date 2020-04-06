d70fe65599fc635ce13629a847cb9c1c915cf8ff
carden <carden.ruby@gmail.com>
2020-04-06 18:25:13 +0300

    fix bugs

--- 07_add_last_if_odd.rb
+++ 07_add_last_if_odd.rb
@@ -2,8 +2,6 @@
 # The first and last elements of the array do not change.
 arr = [1, 2, 2, -4, 0, 6, 7, 8, 12]
 arr = arr.each_with_index do |elem, index|
-  if elem.odd? && elem != arr.first && elem != arr.last
-    arr[index] += arr.last
-  end
+  arr[index] += arr.last if elem.odd? && elem != arr.first && elem != arr.last
 end
 p arr
