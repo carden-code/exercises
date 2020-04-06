b5ca00a51f18473ed1b9012cec1f15d149e81369
carden <carden.ruby@gmail.com>
2020-04-06 20:25:33 +0300

    rename variable

--- 28_elem_located_defor_min_move_end.rb
+++ 28_elem_located_defor_min_move_end.rb
@@ -1,12 +1,12 @@
 # elements located before the minimum, move to the end of the array
 # Дан целочисленный массив. Необходимо разместить элементы,
 # расположенные до минимального, в конце массива.
-arr = [1, 0, 6, 7, 8, -9, 12, -2, 2]
-a = []
-arr.each do |elem|
-  a << elem
-  break if elem == arr.min
+array = [1, 0, 6, 7, 8, -9, 12, -2, 2]
+before_min = []
+array.each do |elem|
+  before_min << elem
+  break unless elem != array.min
 end
-a.pop
-arr.push(arr.shift(a.size))
-puts arr
+before_min.pop
+array.push(array.shift(before_min.size))
+puts "Product: #{array.flatten}"
