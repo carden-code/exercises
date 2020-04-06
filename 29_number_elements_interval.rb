14ae67f998bdb2dc78bdea0ddb42498f3a91fce6
carden <carden.ruby@gmail.com>
2020-04-06 21:28:54 +0300

    fix bugs

--- 29_number_elements_interval.rb
+++ 29_number_elements_interval.rb
@@ -2,6 +2,7 @@
 # Необходимо найти количество элементов в этом интервале.
 # The program finds the number of elements in this interval.
 array = [1, -10, 2, -4, 0, 6, 7, 8, -9, 12, -2, 2]
-interval = 'a'..'b'
-puts "Integer array: #{array}"
-puts "Number of elements in this interval 'a'..'b': #{interval.to_a.size}"
+interval = -5..7
+product = []
+array.each { |element| product << element if interval.include? element }
+puts "Number of elements in the interval: #{product.size}"
