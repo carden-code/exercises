e3b1694d241b6204bcac3ca78edf44178844d6b8
carden <carden.ruby@gmail.com>
2020-04-06 20:24:28 +0300

    rename variable

--- 26_one_element_different.rb
+++ 26_one_element_different.rb
@@ -1,8 +1,10 @@
 # Дан целочисленный массив, в котором лишь один элемент отличается от остальных.
 # Необходимо найти значение этого элемента.
 # The program finds the value of one element that is different from all
-arr = [1, 1, 1, -5, 1, 1, 1, 1]
-w = []
-arr.each_with_index { |elem, index| w << elem if arr[index] == arr[index - 1] }
-arr.delete(w.first)
-puts "Product: #{arr}"
+array = [1, 1, 1, -5, 1, 1, 1, 1]
+duplicate = []
+array.each_with_index do |element, index|
+  duplicate << element unless array[index] != array[index - 1]
+end
+array.delete(duplicate.first)
+puts "Product: #{array}"
