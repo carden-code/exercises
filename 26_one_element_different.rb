a876d62be566bfe379f126b83e9c6e030030f1cd
carden <carden.ruby@gmail.com>
2020-04-06 10:35:42 +0300

    bug fixs

--- 26_one_element_different.rb
+++ 26_one_element_different.rb
@@ -1,6 +1,6 @@
 # The program finds the value of one element that is different from all
 arr = [1, 1, 1, -5, 1, 1, 1, 1]
-duplicate = []
-arr.each_with_index { |elem, index| c << elem if arr[index] == arr[index - 1] }
-arr.delete(duplicate.first)
+w = []
+arr.each_with_index { |elem, index| w << elem if arr[index] == arr[index - 1] }
+arr.delete(w.first)
 puts "Product: #{arr}"
