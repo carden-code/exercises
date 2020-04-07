856c2f6c75e8e2e9fcfdae810ec97028252531b0
carden <carden.ruby@gmail.com>
2020-04-07 23:22:56 +0300

    modification

--- 30_index_local_min.rb
+++ 30_index_local_min.rb
@@ -5,17 +5,5 @@
 # The program determines whether an element
 # at the specified index with a local minimum.
 array = [1, -10, 2, -4, 0, 6, 7, 8, -9, 12, -2, 2]
-natural_index = 4
-number_index = []
-local_minima = []
-array.each_with_index do |element, index|
-  number_index << element unless index != natural_index
-end
-array.each_with_index do |_, index|
-  local_minima << array[index - 1] if array[index - 1] < array[index]
-end
-if local_minima.include? number_index.first
-  puts "index: #{natural_index} - is a local minimum"
-else
-  puts "index: #{natural_index} - not a local minimum"
-end
+index = 4
+puts 'Yes' if array[index - 1..index].min == array[index]
