2c19cdcbdf9444395400db0c4825a76192fa74ee
carden <carden.ruby@gmail.com>
2020-04-08 22:47:10 +0300

    comments

--- 31_elem_between_first_second_max.rb
+++ 31_elem_between_first_second_max.rb
@@ -1,6 +1,7 @@
 # Дан целочисленный массив. Необходимо найти элементы,
 # расположенные между первым и вторым максимальным.
-arr = [1, -10, 20, -4, 0, 6, 7, 8, -9, 12, -2, 2]
-first_max, second_max = arr.max(2)
-temp = [arr.index(first_max), arr.index(second_max)].sort
-puts arr[temp[0] + 1...temp[1]]
+# The program finds elements located between the first and second maximum.
+array = [1, -10, 20, -4, 0, 6, 7, 8, -9, 12, -2, 2]
+first_max, second_max = array.max(2)
+temp = [array.index(first_max), array.index(second_max)].sort
+puts "Product: #{array[temp[0] + 1...temp[1]]}"
