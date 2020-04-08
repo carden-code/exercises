10e678adf34d31034fb24b99aa9468150dba39c0
carden <carden.ruby@gmail.com>
2020-04-09 00:32:21 +0300

    modification

--- 39_max_elem_in_interval.rb
+++ 39_max_elem_in_interval.rb
@@ -1,5 +1,6 @@
 # Дан целочисленный массив и интервал a..b.
 # Необходимо найти максимальный из элементов в этом интервале.
+# The program finds the maximum of elements in that interval.
 array = [1, -12, 2, -4, 0, 6, 7, 8, -9, -100, 12, -2, 2]
 interval = (-10..15)
-puts interval.select { |elem| array.include? elem }.max
+puts interval.select { |element| array.include? element }.max
