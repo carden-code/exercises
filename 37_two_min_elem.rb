f80a18e396d817c5d5920e00a782e1f5bdf2d4cd
carden <carden.ruby@gmail.com>
2020-04-09 00:20:06 +0300

    first commit

---
+++ 37_two_min_elem.rb
@@ -0,0 +1,4 @@
+# Дан целочисленный массив. Необходимо найти два наименьших элемента.
+# The program finds the two smallest elements of an array.
+array = [1, -12, 2, -4, 0, 6, 7, 8, -9, -100, 12, -2, 2]
+puts "Product: #{array.min(2)}"
