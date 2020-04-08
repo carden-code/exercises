11c0a46d0d21a362a11f65be1177adb7d69fe176
carden <carden.ruby@gmail.com>
2020-04-09 00:03:40 +0300

    first commit

--- 35_elem_after_first_max.rb
+++ 35_elem_after_first_max.rb
@@ -1,4 +1,5 @@
 # Дан целочисленный массив. Необходимо найти элементы,
 # расположенные после первого максимального.
+# The program finds elements located after the first maximum.
 array = [1, -12, 2, -4, 0, 6, 7, 8, -9, -100, 12, -2, 2]
 puts "Product: #{array[array.index(array.max) + 1..-1]}"
