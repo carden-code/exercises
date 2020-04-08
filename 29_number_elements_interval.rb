097ef2604d05ae673fdff7008b82f382ba4b2d77
carden <carden.ruby@gmail.com>
2020-04-08 21:25:39 +0300

    modification

--- 29_number_elements_interval.rb
+++ 29_number_elements_interval.rb
@@ -3,5 +3,4 @@
 # The program finds the number of elements in this interval.
 array = [1, -10, 2, -4, 0, 6, 7, 8, -9, 12, -2, 2]
 interval = -5..7
-product = array.select { |element| interval.include? element }
-puts "The number of elements in this interval: #{product.size}"
+puts array.select { |element| interval.include? element }.size
