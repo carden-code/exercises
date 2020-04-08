9d3b41c191e093fff455b72eedeef5dbaa3510ca
carden <carden.ruby@gmail.com>
2020-04-08 23:25:04 +0300

    modification

--- 32_swap_min_max.rb
+++ 32_swap_min_max.rb
@@ -2,7 +2,7 @@
 # минимальный и максимальный элементы массива.
 # The program swaps minimum and maximum elements of the array.
 array = [1, -12, 2, -4, 0, 6, 7, 8, -9, -10, 12, -2, 2]
-min_elem = array.index(array.min)
-max_elem = array.index(array.max)
-array[max_elem], array[min_elem] = array.minmax
+index_min = array.index(array.min)
+index_max = array.index(array.max)
+array[index_max], array[index_min] = array.minmax
 puts "Product: #{array}"
