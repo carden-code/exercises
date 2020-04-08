62cba75b3906510df26456421631e8216079eaaa
carden <carden.ruby@gmail.com>
2020-04-09 00:23:27 +0300

    first commit

---
+++ 38_two_max_elem.rb
@@ -0,0 +1,4 @@
+# Дан целочисленный массив. Необходимо найти два наибольших элемента.
+# The program finds the two largest elements of the array.
+array = [1, -12, 2, -4, 0, 6, 7, 8, -9, -100, 12, -2, 2]
+puts "Product: #{array.max(2)}"
