95d56dac76d21f0e9b76c86dcada5f1945d6a5a7
carden <carden.ruby@gmail.com>
2020-04-10 12:05:10 +0300

    code refactoring

--- 07_add_last_if_odd.rb
+++ 07_add_last_if_odd.rb
@@ -2,6 +2,8 @@
 # The first and last elements of the array do not change.
 arr = [1, 2, 2, -4, 0, 6, 7, 8, 12]
 arr = arr.each_with_index do |elem, index|
-  arr[index] += arr.last if elem.odd? && elem != arr.first && elem != arr.last
+  next if index.zero? || index == arr.size - 1
+
+  arr[index] += arr.last if elem.odd?
 end
-p arr
+puts "Product: #{arr}"
