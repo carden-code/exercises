7751db1069ef3921888dc86a3ef436801a528421
carden <carden.ruby@gmail.com>
2020-04-10 12:27:11 +0300

    if -> unless

--- 07_add_last_if_odd.rb
+++ 07_add_last_if_odd.rb
@@ -2,7 +2,7 @@
 # The first and last elements of the array do not change.
 arr = [1, 2, 2, -4, 0, 6, 7, 8, 12]
 arr = arr.each_with_index do |elem, index|
-  next if index.zero? || index == arr.size - 1
+  next unless index.positive? && index < arr.size - 1
 
   arr[index] += arr.last if elem.odd?
 end
